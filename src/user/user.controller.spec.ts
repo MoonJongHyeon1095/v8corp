74c9495c8b93f731449ab630d9b911977f4e4f98
MoonJongHyeon1095 <109948801+MoonJongHyeon1095@users.noreply.github.com>
2024-06-10 12:51:04 +0900

    test_user: 서비스 로직 수정에 따라 테스트 코드 수정

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,84 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
+    const userServiceMock = {
+      validateUser: jest.fn().mockResolvedValue({
+        userId: 1,
+        username: 'testUser',
+        role: 'user',
+      }),
+      login: jest.fn().mockResolvedValue({
+        accessToken: 'access-token',
+        refreshToken: 'refresh-token',
+      }),
+      createUser: jest.fn().mockResolvedValue({
+        userId: 1,
+        username: 'testUser',
+      }),
+      refresh: jest.fn().mockResolvedValue({
+        accessToken: 'new-access-token',
+        refreshToken: 'new-refresh-token',
+      }),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [{ provide: UserService, useValue: userServiceMock }],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
+  it('UserController가 정의되어 있어야 함', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('로그인 요청시 유효한 토큰을 반환해야 함', async () => {
+    const loginDto = { username: 'testUser', password: 'testPassword' };
+    const response = await controller.login(loginDto);
+
+    expect(userService.validateUser).toHaveBeenCalledWith(
+      'testUser',
+      'testPassword',
+    );
+    expect(response).toEqual({
+      accessToken: 'access-token',
+      refreshToken: 'refresh-token',
+    });
+  });
+
+  it('회원가입 요청시 새로운 사용자 정보를 반환', async () => {
+    const signupDto = { username: 'testUser', password: 'testPassword' };
+    const response = await controller.signup(signupDto);
+
+    expect(userService.createUser).toHaveBeenCalledWith(signupDto);
+    expect(response).toEqual({
+      userId: 1,
+      username: 'testUser',
+    });
+  });
+
+  it('엑세스 토큰 재발급 요청시 새로운 토큰을 반환해야 함', async () => {
+    const mockRequest = {
+      user: {
+        userId: 1,
+        username: 'testUser',
+        role: 'user',
+      },
+    };
+    const response = await controller.refresh(mockRequest as any);
+
+    expect(userService.refresh).toHaveBeenCalledWith(mockRequest.user);
+    expect(response).toEqual({
+      accessToken: 'new-access-token',
+      refreshToken: 'new-refresh-token',
+    });
   });
 });
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,8 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { UserRepository } from './user.repository';
-import { JwtService } from '@nestjs/jwt';
+import { JwtService, JwtSignOptions } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { ConflictException, UnauthorizedException } from '@nestjs/common';
 
 jest.mock('bcrypt', () => ({
   hash: jest.fn().mockResolvedValue('hashedPassword'),
@@ -16,22 +17,27 @@ describe('UserService', () => {
 
   beforeEach(async () => {
     userRepositoryMock = {
+      //호출될 때마다 미리 정의된 값을 반환
       createUser: jest.fn().mockResolvedValue({
         userId: 1,
         username: 'testUser',
         password: 'hashedPassword',
       }),
-      findByUsername: jest.fn().mockResolvedValue({
-        userId: 1,
-        username: 'testUser',
-        password: 'hashedPassword',
-        role: 'normal',
+      //호출될 때 주어진 함수를 실행
+      findByUsername: jest.fn().mockImplementation((username) => {
+        if (username === 'existingUser')
+          return Promise.resolve({
+            userId: 2,
+            username,
+            password: 'hashedPassword',
+            role: 'normal',
+          });
+        return null;
       }),
       updateRefreshToken: jest.fn().mockResolvedValue(true),
     };
-
     jwtServiceMock = {
-      sign: jest.fn().mockReturnValue('mockToken'),
+      sign: jest.fn(() => 'mockToken'),
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -56,7 +62,14 @@ describe('UserService', () => {
   });
 
   describe('createUser', () => {
-    it('새 유저 생성', async () => {
+    it('유저이름 중복', async () => {
+      const signupDto = { username: 'existingUser', password: 'testPass' };
+      await expect(service.createUser(signupDto)).rejects.toThrow(
+        ConflictException,
+      );
+    });
+
+    it('회원가입', async () => {
       const signupDto = { username: 'testUser', password: 'testPass' };
       const result = await service.createUser(signupDto);
 
@@ -70,46 +83,19 @@ describe('UserService', () => {
         username: 'testUser',
       });
     });
-
-    it('회원가입 에러', async () => {
-      jest.spyOn(bcrypt, 'hash').mockRejectedValue(new Error('Hashing failed'));
-      const signupDto = { username: 'testUser', password: 'testPass' };
-
-      await expect(service.createUser(signupDto)).rejects.toThrow(
-        'Hashing failed',
-      );
-    });
   });
 
   describe('validateUser', () => {
-    it('비밀번호 비교 검증', async () => {
-      jest.spyOn(bcrypt, 'compare').mockResolvedValue(true);
-      const result = await service.validateUser('testUser', 'testPass');
-
-      expect(result).toEqual({
-        userId: 1,
-        username: 'testUser',
-        role: 'normal',
-      });
-    });
-
-    it('유저가 조회되지 않으면 null 응답', async () => {
-      jest.spyOn(userRepositoryMock, 'findByUsername').mockResolvedValue(null);
-      const result = await service.validateUser('testUser', 'testPass');
-
-      expect(result).toBeNull();
-    });
-
-    it('비밀번호가 일치하지 않으면 null 반환', async () => {
+    it('비번 일치하지 않으면 401', async () => {
       jest.spyOn(bcrypt, 'compare').mockResolvedValue(false);
-      const result = await service.validateUser('testUser', 'wrongPass');
-
-      expect(result).toBeNull();
+      await expect(
+        service.validateUser('testUser', 'wrongPass'),
+      ).rejects.toThrow(UnauthorizedException);
     });
   });
 
   describe('login', () => {
-    it('accessToken, refreshToken 생성', async () => {
+    it('로그인 성공시 토큰 발급', async () => {
       const validatedUserDto = {
         userId: 1,
         username: 'testUser',
@@ -118,11 +104,33 @@ describe('UserService', () => {
 
       const result = await service.login(validatedUserDto);
 
-      expect(result).toEqual({
-        accessToken: 'Bearer mockToken',
-        refreshToken: 'Bearer mockToken',
+      expect(jwtServiceMock.sign).toHaveBeenCalledWith(expect.any(Object), {
+        expiresIn: '60m',
       });
-      expect(jwtServiceMock.sign).toHaveBeenCalledTimes(2);
+      expect(jwtServiceMock.sign).toHaveBeenCalledWith(expect.any(Object), {
+        expiresIn: '7d',
+      });
+      expect(result.accessToken).toContain('mockToken');
+      expect(result.refreshToken).toContain('mockToken');
+    });
+  });
+
+  describe('refresh', () => {
+    it('토큰 재발급', async () => {
+      const validatedUserDto = {
+        userId: 1,
+        username: 'testUser',
+        role: 'normal',
+      };
+
+      const result = await service.refresh(validatedUserDto);
+
+      expect(result.accessToken).toContain('mockToken');
+      expect(result.refreshToken).toContain('mockToken');
+      expect(userRepositoryMock.updateRefreshToken).toHaveBeenCalledWith(
+        1,
+        expect.any(String),
+      );
     });
   });
 });
