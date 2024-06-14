95cfe9382bdc66441e1f7be221ed0e857218eda0
MoonJongHyeon1095 <109948801+MoonJongHyeon1095@users.noreply.github.com>
2024-06-14 15:19:54 +0900

    feat_libs: add throttle

--- libs/common/src/common.module.ts
+++ libs/common/src/common.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CommonService } from './common.service';
-
+import { ThrottlerModule } from '@nestjs/throttler';
 @Module({
+  imports: [
+    //1분 10회 제한
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+  ],
   providers: [CommonService],
   exports: [CommonService],
 })
